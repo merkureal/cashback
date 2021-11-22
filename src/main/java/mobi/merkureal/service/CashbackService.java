d94650e02a33035b8ea13ba93f5fbd9ad4736299
merkureal <merkureal@outlook.com>
2021-11-22 22:05:21 +0300

    Refactored code in CashbackService

--- src/main/java/mobi/merkureal/service/CashbackService.java
+++ src/main/java/mobi/merkureal/service/CashbackService.java
@@ -3,9 +3,6 @@ package mobi.merkureal.service;
 public class CashbackService {
     public int calculate(int purchase) {
         int cashback = purchase * 5 / 100;
-        if (cashback > 3_000) {
-            return 3_000;
-        }
-        return cashback;
+        return Math.min(cashback, 3_000);
     }
 }
